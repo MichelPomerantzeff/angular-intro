76b328958fed087554b2e0c5a4fffd9ac5e1aa2a
Michel Pomerantzeff <micheldauniao@hotmail.com>
2023-03-02 20:53:09 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TopbarComponent,
+    TasksComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() 'task': Task;
+
+  ngOnInit(): void {}
+}
